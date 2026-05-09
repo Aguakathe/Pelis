7acbd26016c4122a26a26b40b87e66a825e549a0
Aguakathe <sktorres6@gmail.com>
2026-05-09 14:09:50 -0500

    Creación y estilos del nuevo componente (CTA-BANNER)

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { TarjetasTendenciaComponent } from '../../components/tarjetas-tendencia/tarjetas-tendencia.component';
 import { CategoriesComponent } from '../../components/categories/categories.component';
+import { CtaBannerComponent } from '../../components/cta-banner/cta-banner.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeroComponent, TarjetasTendenciaComponent, CategoriesComponent],
+  imports: [HeroComponent, TarjetasTendenciaComponent, CategoriesComponent, CtaBannerComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
