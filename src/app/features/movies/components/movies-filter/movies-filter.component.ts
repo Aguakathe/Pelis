18a00725ed647e4b69f439451689df970c913148
Aguakathe <sktorres6@gmail.com>
2026-05-23 11:02:33 -0500

    Creación de componentes

---
+++ src/app/features/movies/components/movies-filter/movies-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-filter',
+  imports: [],
+  templateUrl: './movies-filter.component.html',
+  styleUrl: './movies-filter.component.css'
+})
+export class MoviesFilterComponent {
+
+}
